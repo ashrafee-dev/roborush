96c3726bf571485479ca1eab99dffdc0182dd49c
Mahdi Ashrafee <ashrafeem8@gmail.com>
2025-11-08 16:45:30 -0500

    feat: add logic for robots operation

--- robot_control/robot_script.py
+++ robot_control/robot_script.py
@@ -1,11 +1,38 @@
 from elegoo_controller import ElegooRobotController
 import time
 
+
+def main():
+    raw = sys.stdin.read()
+    data = json.loads(raw) 
+    location = data.get(location).lower()
+
+
+
 print("Connecting to robot...")
 with ElegooRobotController() as robot:
-    print("Moving forward 2 seconds...")
-    robot.forward(speed=80, duration=1.0)
-    time.sleep(2.5)
+
+    if location == "knox":
+        robot.forward(speed=80, duration=1.0)
+        time.sleep(2.5)
+    elif location == "davis":
+        robot.forward(speed=80, duration=3.0)
+        time.sleep(2.5)
+    elif location == "capen":
+        robot.forward(speed=80, duration=5.0)
+        time.sleep(2.5)
+    elif location == "clemens":
+        robot.forward(speed=80, duration=7.0)
+        time.sleep(2.5)
+    elif location == "park":
+        robot.forward(speed=80, duration=9.0)
+        time.sleep(2.5)
+    elif location == "lockwood":
+        robot.forward(speed=80, duration=11.0)
+        time.sleep(2.5)
+    else:
+        sys.exit("invalid location")
+
     
   
     #robot.turn_90_degrees(clockwise=True)
@@ -18,3 +45,6 @@ with ElegooRobotController() as robot:
         robot.stop()
 
 print("Done!")
+
+if __name__ == "__main__":
+    main()
